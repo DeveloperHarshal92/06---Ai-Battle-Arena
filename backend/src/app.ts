e17660b40ec40b613ddf1ffef0e08a1dd2c281bf
Harshal Varade <developer.ever@gmail.com>
2026-04-03 19:48:57 +0530

    feat: add langchain and zod dependencies, implement graph-based AI solution

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,17 @@
 import express from "express";
+import useGraph from "./services/graph.ai.service.js"
 
 const app = express();
 
 app.get("/health", (req, res) => {
   res.status(200).json({ status: "ok" });
 });
+
+app.post("/use-graph",async(req,res)=>{
+  await useGraph("Write an factorial function in javascript ")
+  res.status(200).json({
+    message : "Request send successfully."
+  })
+})
 
 export default app;
